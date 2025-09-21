25a8e3a31ee91807f076dbb6e5c4a8faa2ba9701
Joy Krisno Ray <joykrisnoray@gmail.com>
2025-09-21 15:37:32 +0600

    Update

--- FinancialManagement.API/Program.cs
+++ FinancialManagement.API/Program.cs
@@ -105,7 +105,7 @@ namespace FinancialManagement.API
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Financial Management API V1");
-                    c.RoutePrefix = string.Empty; // Set Swagger UI at app's root
+                   // c.RoutePrefix = string.Empty; // Set Swagger UI at app's root
                 });
             }
 
